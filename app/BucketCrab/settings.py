d08379c101b737dff2a178f9fd795068b1d1c772
mohit4 <mohitkumar2801@gmail.com>
2021-06-02 16:43:47 +0530

    Completed accounts app

--- app/BucketCrab/settings.py
+++ app/BucketCrab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'account',
 ]
 
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'BucketCrab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +128,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Parameters for defining Bootstrap message classes against each type of message constraints
+MESSAGE_TAGS = {
+    messages.DEBUG : 'alert-info',
+    messages.INFO : 'alert-info',
+    messages.SUCCESS : 'alert-success',
+    messages.WARNING : 'alert-warning',
+    messages.ERROR : 'alert-danger'
+}
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- app/account/models.py
+++ app/account/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.urls import reverse
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    A user profile containing all user details
+    """
+
+    bio = models.TextField(null=True, help_text="Few words about you...")
+    website = models.URLField(null=True, help_text="Your personal website.")
+
+    coins = models.PositiveBigIntegerField(default=100, help_text="Coins for trading from market place.")
+    crab_eggs = models.PositiveBigIntegerField(default=5, help_text="Crab eggs are used to plant tasks in Buckets.")
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return f"{self.user.username} profile"
+    
+    def get_absolute_url(self):
+        return reverse("account:profile-detail", kwargs={"pk":self.pk})
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
